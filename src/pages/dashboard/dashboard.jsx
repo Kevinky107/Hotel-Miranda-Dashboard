2a0195370e74d7e5fda5fa3fe5da15d99f357b8f
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-06-25 18:11:54 +0200

    ADD: information of dashboard

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,15 +1,53 @@
 import { useContext, useEffect, useState } from 'react'
 import './dashboard.css'
 import { ThemeContext } from '../../App';
+import { IoBedOutline } from "react-icons/io5";
+import { LuCalendarCheck2 } from "react-icons/lu";
+import { IoLogOutOutline, IoLogInOutline } from "react-icons/io5";
 
 function Dashboard() {
 
-  
+  const themeSelector = useContext(ThemeContext)
 
   return (
-    <>
-        
-    </>
+    <div className={`dashboard-`+themeSelector}>
+        <div className='facts'>
+          <picture>
+            <IoBedOutline />
+          </picture>
+          <div>
+            <h3></h3>
+            <h4>Bookings</h4>
+          </div>
+        </div>
+        <div className='facts'>
+          <picture className='picture2'>
+            <LuCalendarCheck2 />
+          </picture>
+          <div>
+            <h3></h3>
+            <h4>Occupation</h4>
+          </div>
+        </div>
+        <div className='facts'>
+          <picture>
+            <IoLogOutOutline />
+          </picture>
+          <div>
+            <h3></h3>
+            <h4>Check In</h4>
+          </div>
+        </div>
+        <div className='facts'> 
+          <picture>
+            <IoLogInOutline />
+          </picture>
+          <div>
+            <h3></h3>
+            <h4>Check Out</h4>
+          </div>
+        </div>
+    </div>
   )
 }
 
