f0bcf8c1bf975943ead96f77d7bce39b9091fb62
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-06-25 10:02:44 +0200

    ADD: logo and footer

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -2,6 +2,13 @@ import { useContext, useState } from 'react'
 import './dashboard.css'
 import { ThemeContext } from '../../App';
 import { Outlet } from 'react-router-dom';
+import { LuLayoutDashboard } from "react-icons/lu";
+import { BiKey } from "react-icons/bi";
+import { LuCalendarCheck2 } from "react-icons/lu";
+import { IoPersonOutline } from "react-icons/io5";
+import { PiPuzzlePiece } from "react-icons/pi";
+import { PiBuildingFill } from "react-icons/pi";
+import { GiStarsStack } from "react-icons/gi";
 
 function Dashboard() {
 
@@ -11,20 +18,33 @@ function Dashboard() {
   return (
     <main className={`main-`+themeSelector}>
         <aside>
+            <picture class='logo'>
+                <PiBuildingFill className='build'/>
+                <GiStarsStack className='stars'/>
+                <div>
+                    <h1>travl</h1>
+                    <h4>Hotel Admin Dashboard</h4>
+                </div>
+            </picture>
             <ul>
                 <li>
+                    <LuLayoutDashboard />
                     <h3>Dashboard</h3>
                 </li>
                 <li>
+                    <BiKey />
                     <h3>Room</h3>
                 </li>
                 <li>
+                    <LuCalendarCheck2 />
                     <h3>Bookings</h3>
                 </li>
                 <li>
+                    <IoPersonOutline />
                     <h3>Guest</h3>
                 </li>
                 <li>
+                    <PiPuzzlePiece />
                     <h3>Concierge</h3>
                 </li>
             </ul>
@@ -33,6 +53,11 @@ function Dashboard() {
                 <h4>{user.name+' '+user.lastName}</h4>
                 <p>{user.email}</p>
                 <button>Edit</button>
+            </div>
+            <div className='footer'>
+                <h3>Travl Hotel Admin Dashboard</h3>
+                <h4>© 2024 All Rights Reserved</h4>
+                <p>Made with ♥ by Kevin</p>
             </div>
         </aside>
         <Outlet/>
