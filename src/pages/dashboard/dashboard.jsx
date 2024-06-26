bac737574a11bccfacff48a6cf159a785e59b48f
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-06-26 09:10:40 +0200

    ADD: info in dasboard

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -4,10 +4,19 @@ import { ThemeContext } from '../../App';
 import { IoBedOutline } from "react-icons/io5";
 import { LuCalendarCheck2 } from "react-icons/lu";
 import { IoLogOutOutline, IoLogInOutline } from "react-icons/io5";
+import booking from '../../assets/bookings.json'
+import rooms from '../../assets/rooms.json'
 
 function Dashboard() {
 
   const themeSelector = useContext(ThemeContext)
+
+  const bookings = booking.length
+  const numroom = rooms.length
+  const occupied = rooms.filter((room) => room.available === false).length
+  const occupation = ((occupied/numroom)*100).toFixed(2) + "%"
+  const checkin = booking.filter((element) => element.status === "check in" || element.status === "check out").length
+  const checkout = booking.filter((element) => element.status === "check out").length
 
   return (
     <div className={`dashboard-`+themeSelector}>
@@ -16,7 +25,7 @@ function Dashboard() {
             <IoBedOutline />
           </picture>
           <div>
-            <h3></h3>
+            <h3>{bookings}</h3>
             <h4>Bookings</h4>
           </div>
         </div>
@@ -25,7 +34,7 @@ function Dashboard() {
             <LuCalendarCheck2 />
           </picture>
           <div>
-            <h3></h3>
+            <h3>{occupation}</h3>
             <h4>Occupation</h4>
           </div>
         </div>
@@ -34,7 +43,7 @@ function Dashboard() {
             <IoLogOutOutline />
           </picture>
           <div>
-            <h3></h3>
+            <h3>{checkin}</h3>
             <h4>Check In</h4>
           </div>
         </div>
@@ -43,7 +52,7 @@ function Dashboard() {
             <IoLogInOutline />
           </picture>
           <div>
-            <h3></h3>
+            <h3>{checkout}</h3>
             <h4>Check Out</h4>
           </div>
         </div>
