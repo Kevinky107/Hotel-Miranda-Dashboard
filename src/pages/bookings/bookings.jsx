18d4af8dae1c5678571cf61835e31e6fe0b70f71
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-06-26 12:07:27 +0200

    FIX: pages documents

---
+++ src/pages/bookings/bookings.jsx
@@ -0,0 +1,16 @@
+import { useContext, useEffect, useState } from 'react'
+import './bookings.css'
+import { ThemeContext } from '../../App';
+
+function Bookings() {
+
+  
+
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Bookings
---
+++ src/pages/concierge/concierge.jsx
@@ -0,0 +1,16 @@
+import { useContext, useEffect, useState } from 'react'
+import './concierge.css'
+import { ThemeContext } from '../../App';
+
+function Concierge() {
+
+  
+
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Concierge
