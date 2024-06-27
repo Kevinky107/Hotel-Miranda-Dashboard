540ffa309d0aa42b67e0c7d7470fb7d029b3d6d6
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-06-27 12:31:15 +0200

    ADD: room HTML completed

--- src/pages/room/room.jsx
+++ src/pages/room/room.jsx
@@ -3,11 +3,12 @@ import { ThemeContext } from '../../App';
 import { RoomContainer } from '../../components/room-styled';
 import { Column, ColumnTitle, Row, Table, TableBody } from '../../components/table-styled';
 import rooms from '../../assets/rooms.json'
+import { GreenButton } from '../../components/button-styled';
 
 function Room() {
 
   const themeSelector = useContext(ThemeContext)
-  const pageSize = 6
+  const pageSize = 10
 
   const createPagination = (array, size) => {
     const aux = []
@@ -17,44 +18,73 @@ function Room() {
     return aux 
   }
 
-  const [roomList, setRoomList] = useState(createPagination(rooms, pageSize))
+  const [list, setList] = useState(rooms)
+  const [roomPages, setRoomPages] = useState(createPagination(list, pageSize))
   const [page, setPage] = useState(0)
+
+  const allRooms = () => {
+    setPage(0)
+    setList(rooms)
+    setRoomPages(createPagination(rooms, pageSize))
+  }
+
+  const availableRooms = () => {
+    const aux = rooms.filter((room) => room.available)
+    setPage(0)
+    setList(aux)
+    setRoomPages(createPagination(aux, pageSize))
+  }
+
+  const bookedRooms = () => {
+    const aux = rooms.filter((room) => !room.available)
+    setPage(0)
+    setList(aux)
+    setRoomPages(createPagination(aux, pageSize))
+  }
 
   return (
     <RoomContainer>
+      <div>
+        <h2 onClick={allRooms}>All Rooms</h2>
+        <h2 onClick={availableRooms}>Available Rooms</h2>
+        <h2 onClick={bookedRooms}>Booked Rooms</h2>
+      </div>
+      <GreenButton theme="dark">+ New Room</GreenButton>
       <Table theme={themeSelector}>
         <TableBody>
           <Row>
             <ColumnTitle>Room Name</ColumnTitle>
             <ColumnTitle>Bed Type</ColumnTitle>
-            <ColumnTitle>Room Floor</ColumnTitle>
             <ColumnTitle>Amenities</ColumnTitle>
             <ColumnTitle>Price</ColumnTitle>
+            <ColumnTitle>Offer</ColumnTitle>
+            <ColumnTitle>Status</ColumnTitle>
           </Row>
           {
-            roomList[page].map((room, index) => 
+            roomPages[page].map((room, index) => 
               <Row key={index}>
-                <Column>{room.id}</Column>
+                <Column>#{room.id}</Column>
                 <Column>{room.roomtype}</Column>
-                <Column>{room.name}</Column>
                 <Column>{room.Amenities}</Column>
+                <Column>${room.price}/Night</Column>
                 <Column>${room.offer}/Night</Column>
+                <Column>{room.available ? "Available" : "Booked"}</Column>
               </Row>
             )
           }
         </TableBody>
       </Table>
       <div>
-        <h4>Showing {pageSize * (page+1)} of {rooms.length}</h4>
+        <h4>Showing {pageSize * (page) + roomPages[page].length} of {list.length}</h4>
       </div>
       <div>
-        <button onClick={() => page > 0 && setPage(page-1)}>prev</button>
+        <button onClick={() => page > 0 && setPage(page-1)}>Prev</button>
         {
-          roomList.map((array, index) =>
-            (index === page || index >= page-2 && index <= page+2 ) && <button onClick={() => setPage(index)}>{index+1}</button> 
+          roomPages.map((array, index) =>
+            (index === page || index >= page-2 && index <= page+2 ) && <button key={index} onClick={() => setPage(index)}>{index+1}</button> 
           )
         }
-        <button onClick={() => page+1 < roomList.length && setPage(page+1)}>next</button>
+        <button onClick={() => page+1 < roomPages.length && setPage(page+1)}>Next</button>
       </div>
     </RoomContainer>
   )
