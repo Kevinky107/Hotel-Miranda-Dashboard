3abbac8b2f8656c10faf6a82ff2127db51e2cdc9
Kevin Agudo Montil <121159576+Kevinky107@users.noreply.github.com>
2024-07-01 09:16:52 +0200

    ADD: form labels

--- src/pages/editUser.jsx
+++ src/pages/editUser.jsx
@@ -1,30 +1,44 @@
 import { useContext, useEffect, useState } from 'react'
 import { PageContainer } from '../components/pageStyled'
 import { ThemeContext } from '../App'
+import { FormStyledWrapper } from '../components/formStyled'
 
 function EditUser() {
 
   const themeSelector = useContext(ThemeContext)
+  const user = JSON.parse(localStorage.getItem('user'))
 
   return (
     <PageContainer>
-      <form>
-        <input type='file' />
-        <input type='text' />
-        <select>
-          <option>Manager</option>
-          <option>Reception</option>
-          <option>Room Service</option>
-        </select>
-        <input type='email' />
-        <input type='tel' />
-        <input type='date' />
-        <input type='text' />
-        <input type='text' />
-        <input type='checkbox' />
-        <input type='text' />
-        <input type='submit' value='SAVE CHANGES'/>
-      </form>
+      <FormStyledWrapper>
+        <form>
+          <h4>Images</h4>
+          <input type='file' />
+          <h4>Name</h4>
+          <input type='text' value={user.name} />
+          <h4>Job</h4>
+          <select>
+            <option>Manager</option>
+            <option>Reception</option>
+            <option>Room Service</option>
+          </select>
+          <h4>Email</h4>
+          <input type='email' value={user.email} />
+          <h4>Phone Number</h4>
+          <input type='tel' value={user.phone} />
+          <h4>Start Date</h4>
+          <input type='date' value={user.startdate} />
+          <h4>Job Description</h4>
+          <input type='text' value={user.postdescription} />
+          <h4>Active</h4>
+          {user.state ?
+            <input type='checkbox' checked/> 
+            : 
+            <input type='checkbox'/>
+          }
+          <input type='submit' value='SAVE CHANGES'/>
+        </form>
+      </FormStyledWrapper>
     </PageContainer>
   )
 }
--- src/pages/newEmployee.jsx
+++ src/pages/newEmployee.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react'
 import { PageContainer } from '../components/pageStyled'
 import { ThemeContext } from '../App'
+import { FormStyledWrapper } from '../components/formStyled'
 
 function NewEmployee() {
 
@@ -8,23 +9,31 @@ function NewEmployee() {
 
   return (
     <PageContainer>
+      <FormStyledWrapper>
       <form>
-        <input type='file' />
-        <input type='text' />
-        <select>
-          <option>Manager</option>
-          <option>Reception</option>
-          <option>Room Service</option>
-        </select>
-        <input type='email' />
-        <input type='tel' />
-        <input type='date' />
-        <input type='text' />
-        <input type='text' />
-        <input type='checkbox' />
-        <input type='text' />
-        <input type='submit' value='ADD EMPLOYEE'/>
-      </form>
+          <h4>Images</h4>
+          <input type='file' />
+          <h4>Name</h4>
+          <input type='text' />
+          <h4>Job</h4>
+          <select>
+            <option>Manager</option>
+            <option>Reception</option>
+            <option>Room Service</option>
+          </select>
+          <h4>Email</h4>
+          <input type='email' />
+          <h4>Phone Number</h4>
+          <input type='tel' />
+          <h4>Start Date</h4>
+          <input type='date' />
+          <h4>Job Description</h4>
+          <input type='text' />
+          <h4>Active</h4>
+          <input type='checkbox' checked/> 
+          <input type='submit' value='SAVE CHANGES'/>
+        </form>
+      </FormStyledWrapper>
     </PageContainer>
   )
 }
